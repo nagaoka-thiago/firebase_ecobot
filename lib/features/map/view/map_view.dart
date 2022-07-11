911bce1dd3edfe209c9a4c6f29cd7fa0d1b7fe75
Thiago Nagaoka <nagaoka.thiago@bol.com.br>
2022-07-11 10:23:27 -0300

    added mapbox on~map page, need to adapt to mobx

--- lib/features/map/view/map_view.dart
+++ lib/features/map/view/map_view.dart
@@ -1,8 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_dragmarker/dragmarker.dart';
+import 'package:flutter_map_line_editor/polyeditor.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class MapView extends StatelessWidget {
+class MapView extends StatefulWidget {
   const MapView({Key? key}) : super(key: key);
+
+  @override
+  State<MapView> createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  late PolyEditor _polyEditor;
+  List<Polygon> _polygons = [];
+  late Location location;
+  LatLng firstLocation = LatLng(45, 45);
+
+  var _testPolygon = Polygon(
+      color: Colors.red,
+      points: [],
+      borderStrokeWidth: 5,
+      borderColor: Colors.red);
+
+  @override
+  void initState() {
+    super.initState();
+
+    Permission.location.request();
+    Permission.locationAlways.request();
+    Permission.locationWhenInUse.request();
+    _polyEditor = PolyEditor(
+        points: _testPolygon.points,
+        pointIcon: Icon(Icons.location_on, size: 23, color: Colors.red),
+        addClosePathMarker: true,
+        intermediateIcon: Icon(Icons.lens, size: 15, color: Colors.red),
+        callbackRefresh: () {
+          setState(() {});
+        });
+    _polygons.add(_testPolygon);
+
+    location = Location();
+
+    location.getLocation().then((loc) => {
+          setState(() {
+            firstLocation = LatLng(loc.latitude!, loc.longitude!);
+          })
+        });
+
+    location.onLocationChanged.listen((event) {
+      setState(() {
+        firstLocation = LatLng(event.latitude!, event.longitude!);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +63,54 @@ class MapView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Map'),
       ),
-      body: MapboxMap(
-        accessToken:
-            'pk.eyJ1IjoidGhpYWdvLW5hZ2Fva2EiLCJhIjoiY2w1YXgxY2NnMDIwcjNqcXNlbzk2YjhkdSJ9.RHK_i-FMPZ0qg3Bnk6cQ4w',
-        styleString: 'mapbox://styles/thiago-nagaoka/cl5cfhwav000614mowkhsgznp',
-        initialCameraPosition: CameraPosition(
-          zoom: 15.0,
-          target: LatLng(14.508, 46.048),
+      body: Center(
+        child: FlutterMap(
+          options: MapOptions(
+            allowPanningOnScrollingParent: false,
+            center: firstLocation,
+            onTap: (_, p) {
+              setState(() {
+                _polyEditor.add(_testPolygon.points, p);
+              });
+            },
+            plugins: [
+              DragMarkerPlugin(),
+            ],
+            zoom: 13.0,
+          ),
+          layers: [
+            TileLayerOptions(
+                urlTemplate:
+                    'https://api.mapbox.com/styles/v1/thiago-nagaoka/cl5cfhwav000614mowkhsgznp/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGhpYWdvLW5hZ2Fva2EiLCJhIjoiY2w1YXgxY2NnMDIwcjNqcXNlbzk2YjhkdSJ9.RHK_i-FMPZ0qg3Bnk6cQ4w',
+                additionalOptions: {
+                  'accessToken':
+                      'pk.eyJ1IjoidGhpYWdvLW5hZ2Fva2EiLCJhIjoiY2w1YXgxY2NnMDIwcjNqcXNlbzk2YjhkdSJ9.RHK_i-FMPZ0qg3Bnk6cQ4w',
+                  'id': 'mapbox.mapbox-streets-v8'
+                }),
+            MarkerLayerOptions(markers: [
+              Marker(
+                  point: firstLocation,
+                  builder: (context) {
+                    return Icon(
+                      Icons.location_on_outlined,
+                      color: Colors.red,
+                    );
+                  })
+            ]),
+            PolygonLayerOptions(
+              polygons: _polygons,
+            ),
+            DragMarkerPluginOptions(markers: _polyEditor.edit()),
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _testPolygon.points.removeLast();
+            });
+          },
+          child: Icon(Icons.delete)),
     );
   }
 }
