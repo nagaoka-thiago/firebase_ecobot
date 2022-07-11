7e93ac974ec4a894d459d4e14eb90ea104c3e261
Thiago Nagaoka <nagaoka.thiago@bol.com.br>
2022-07-11 13:55:00 -0300

    trying to fix mobx error on map page

--- lib/features/map/view/map_view.dart
+++ lib/features/map/view/map_view.dart
@@ -5,6 +5,9 @@ import 'package:flutter_map_line_editor/polyeditor.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:location/location.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+import '../controller/map_view_controller.dart';
 
 class MapView extends StatefulWidget {
   const MapView({Key? key}) : super(key: key);
@@ -14,16 +17,7 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
-  late PolyEditor _polyEditor;
-  List<Polygon> _polygons = [];
-  late Location location;
-  LatLng firstLocation = LatLng(45, 45);
-
-  var _testPolygon = Polygon(
-      color: Colors.red,
-      points: [],
-      borderStrokeWidth: 5,
-      borderColor: Colors.red);
+  late MapViewController controller;
 
   @override
   void initState() {
@@ -32,29 +26,9 @@ class _MapViewState extends State<MapView> {
     Permission.location.request();
     Permission.locationAlways.request();
     Permission.locationWhenInUse.request();
-    _polyEditor = PolyEditor(
-        points: _testPolygon.points,
-        pointIcon: Icon(Icons.location_on, size: 23, color: Colors.red),
-        addClosePathMarker: true,
-        intermediateIcon: Icon(Icons.lens, size: 15, color: Colors.red),
-        callbackRefresh: () {
-          setState(() {});
-        });
-    _polygons.add(_testPolygon);
 
-    location = Location();
-
-    location.getLocation().then((loc) => {
-          setState(() {
-            firstLocation = LatLng(loc.latitude!, loc.longitude!);
-          })
-        });
-
-    location.onLocationChanged.listen((event) {
-      setState(() {
-        firstLocation = LatLng(event.latitude!, event.longitude!);
-      });
-    });
+    controller = MapViewController();
+    controller.initializeAll();
   }
 
   @override
@@ -64,51 +38,49 @@ class _MapViewState extends State<MapView> {
         title: const Text('Map'),
       ),
       body: Center(
-        child: FlutterMap(
-          options: MapOptions(
-            allowPanningOnScrollingParent: false,
-            center: firstLocation,
-            onTap: (_, p) {
-              setState(() {
-                _polyEditor.add(_testPolygon.points, p);
-              });
-            },
-            plugins: [
-              DragMarkerPlugin(),
-            ],
-            zoom: 13.0,
-          ),
-          layers: [
-            TileLayerOptions(
-                urlTemplate:
-                    'https://api.mapbox.com/styles/v1/thiago-nagaoka/cl5cfhwav000614mowkhsgznp/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGhpYWdvLW5hZ2Fva2EiLCJhIjoiY2w1YXgxY2NnMDIwcjNqcXNlbzk2YjhkdSJ9.RHK_i-FMPZ0qg3Bnk6cQ4w',
-                additionalOptions: {
-                  'accessToken':
-                      'pk.eyJ1IjoidGhpYWdvLW5hZ2Fva2EiLCJhIjoiY2w1YXgxY2NnMDIwcjNqcXNlbzk2YjhkdSJ9.RHK_i-FMPZ0qg3Bnk6cQ4w',
-                  'id': 'mapbox.mapbox-streets-v8'
-                }),
-            MarkerLayerOptions(markers: [
-              Marker(
-                  point: firstLocation,
-                  builder: (context) {
-                    return Icon(
-                      Icons.location_on_outlined,
-                      color: Colors.red,
-                    );
-                  })
-            ]),
-            PolygonLayerOptions(
-              polygons: _polygons,
+        child: Observer(builder: (_) {
+          return FlutterMap(
+            options: MapOptions(
+              allowPanningOnScrollingParent: false,
+              center: controller.firstLocation,
+              onTap: (_, p) {
+                controller.addPoint(p);
+              },
+              plugins: [
+                DragMarkerPlugin(),
+              ],
+              zoom: 13.0,
             ),
-            DragMarkerPluginOptions(markers: _polyEditor.edit()),
-          ],
-        ),
+            layers: [
+              TileLayerOptions(
+                  urlTemplate:
+                      'https://api.mapbox.com/styles/v1/thiago-nagaoka/cl5cfhwav000614mowkhsgznp/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGhpYWdvLW5hZ2Fva2EiLCJhIjoiY2w1YXgxY2NnMDIwcjNqcXNlbzk2YjhkdSJ9.RHK_i-FMPZ0qg3Bnk6cQ4w',
+                  additionalOptions: {
+                    'accessToken':
+                        'pk.eyJ1IjoidGhpYWdvLW5hZ2Fva2EiLCJhIjoiY2w1YXgxY2NnMDIwcjNqcXNlbzk2YjhkdSJ9.RHK_i-FMPZ0qg3Bnk6cQ4w',
+                    'id': 'mapbox.mapbox-streets-v8'
+                  }),
+              MarkerLayerOptions(markers: [
+                Marker(
+                    point: controller.firstLocation,
+                    builder: (context) {
+                      return Icon(
+                        Icons.location_on_outlined,
+                        color: Colors.red,
+                      );
+                    })
+              ]),
+              PolygonLayerOptions(
+                polygons: controller.polygons,
+              ),
+              DragMarkerPluginOptions(markers: controller.polyEditor.edit()),
+            ],
+          );
+        }),
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
-            setState(() {
-              _testPolygon.points.removeLast();
-            });
+            controller.removePoint();
           },
           child: Icon(Icons.delete)),
     );
